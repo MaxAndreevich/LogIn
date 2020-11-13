fc9be2d7de82dc8861476295f7a9b14fb2fa650b
MaxAndreevich <maxanokhin1997@gmail.com>
2020-11-13 14:38:23 +0300

    StartView

--- LogIn/LogIn/Application/SceneDelegate.swift
+++ LogIn/LogIn/Application/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let winScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: winScene)
         window?.makeKeyAndVisible()
-        window?.rootViewController = RecoveryViewController()
+        window?.rootViewController = StartViewController()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
